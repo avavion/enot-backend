02941b25cebe6b78e005f6d4ebe54125b70903ba
Alan Bogov (@avavion) <avavionmvm@gmail.com>
2023-05-14 19:45:12 +0300

    Fix: bash script

--- parse.sh
+++ parse.sh
@@ -1,3 +1,7 @@
 ROOT=$(pwd)
 
-curl -I "http://enot/valutes/parse?email=parser@enot.ru&password=parserparser" >> "$ROOT\logger.txt"
+curl "http://enot/valutes/parse?email=parser@enot.ru&password=parserparser"
+
+current_date_time=$(date)
+
+echo "Current date and time: $current_date_time" >> "$ROOT/logger.txt"
